899754278b8ee402ce2e7e0485982cece8cdb019
yawuztsn <yawuztosun69@gmail.com>
2023-08-18 20:49:15 +0300

    CRUD işlemleri yapıldı
    Northwind veri tabanı üzerinden create,update,delete işlemleri yapıldı

---
+++ MVCNorthwind_3/MVCNorthwind_3/Controllers/CategoryController.cs
@@ -0,0 +1,58 @@
+﻿using MVCNorthwind_3.DesignPatterns.SingletonPattern;
+using MVCNorthwind_3.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCNorthwind_3.Controllers
+{
+    public class CategoryController : Controller
+    {
+        NorthwindEntities _db;
+        public CategoryController()
+        {
+            _db = DBTool.DbInstance;
+        }
+        public ActionResult ListCategories()
+        {
+            return View(_db.Categories.ToList());
+        }
+        public ActionResult AddCategory()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult AddCategory(Category category)
+        {
+            _db.Categories.Add(category);
+            _db.SaveChanges();
+            return RedirectToAction("ListCategories");
+        }
+
+        public ActionResult UpdateCategory(int id) 
+        {
+            return View(_db.Categories.Find(id));
+        }
+
+        [HttpPost]
+        public ActionResult UpdateCategory(Category category)
+        {
+            Category guncellenecek = _db.Categories.Find(category.CategoryID);
+            guncellenecek.CategoryName = category.CategoryName;
+            guncellenecek.Description = category.Description;
+            _db.SaveChanges();
+            return RedirectToAction("ListCategories");
+        }
+
+        public ActionResult DeleteCategory(int id)
+        {
+            _db.Categories.Remove(_db.Categories.Find(id));
+            _db.SaveChanges();
+            return RedirectToAction("ListCategories");
+        }
+
+    }
+}
---
+++ MVCNorthwind_3/MVCNorthwind_3/DesignPatterns/SingletonPattern/DBTool.cs
@@ -0,0 +1,24 @@
+﻿using MVCNorthwind_3.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCNorthwind_3.DesignPatterns.SingletonPattern
+{
+    public class DBTool
+    {
+        DBTool()  { }
+
+        static NorthwindEntities _dbInstance;
+        public static NorthwindEntities DbInstance
+        {
+            get 
+            { 
+                if(_dbInstance==null)_dbInstance = new NorthwindEntities();
+                return _dbInstance;
+            }
+            
+        }
+    }
+}
